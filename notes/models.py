3957dbadf422d7de622da3c680c9079361e79ed9
SHMF1385 <shmf.mousavi1385@gmail.com>
2021-10-25 13:02:16 -0400

    add Note model for user notes

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=24)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.FileField()
+
