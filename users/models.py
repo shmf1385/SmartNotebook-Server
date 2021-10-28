ef2053321496caaa167383ad1e58e300c0e9e672
SHMF1385 <shmf.mousavi1385@gmail.com>
2021-10-28 12:36:37 +0330

    add Token model for auth Users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)
+
+    def __str__(self):
+        return f"{self.user}'s Token"    
